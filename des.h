4ca6775e5d833cfc9ae388a8eaa5476580c6a73d
Kyle Lin <kylelin47@gmail.com>
2015-02-05 17:47:07 -0500

    .

--- des.h
+++ des.h
@@ -3,6 +3,8 @@
 /*
  * Known Limitations:
  * 		Maximum Key Size: 256 bits (binary), due to hexToBin bitset<MAX_KEY_SIZE>. Can be changed.
+ * Brittleness:
+ *		Requires users to have valid inputs
  */
 #include <vector>
 #include <string>
@@ -10,12 +12,14 @@
 #include <bitset>
 #include <iostream>
 
-std::string permute(std::string input, const std::vector<int> & permute);
+std::string permute(std::string input, const std::vector<std::string> & permute);
 std::string XOR(std::string input1, std::string input2);
-std::string cycleLeft(std::string input, int places);
+std::string cycleLeft(std::string input, std::string places);
 std::string splitLeft(std::string input);
 std::string splitRight(std::string input);
 std::string binToHex(std::string binary_input);
 std::string hexToBin(std::string hex_input, unsigned int size);
+std::string decToBin(int decimal_input, unsigned int size);
+int hex_char_to_decimal(std::string c);
 
 #endif
---
+++ makefile
@@ -0,0 +1,2 @@
+%.o: main.cpp des.h
+	g++ -std=c++11 main.cpp des.cpp
